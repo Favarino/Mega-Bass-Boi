dd35961881dacf1ab7cd1c7801c00b702d109da0
Nicole Long <n.long1520@gmail.com>
2018-09-15 15:28:27 -0700

    Indicator Follow Camera

---
+++ Assets/FollowTarget.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour {
+    [SerializeField] Transform target;
+    [SerializeField] float smoothTime = 0.3f;
+
+    Vector3 velocity = Vector3.zero;
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, 10));
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+	}
+}

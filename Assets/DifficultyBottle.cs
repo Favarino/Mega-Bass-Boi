b18631f8fa64369e9dc36176a33eec688ba5da2e
Nicole Long <n.long1520@gmail.com>
2018-09-16 11:08:59 -0700

    Enviroment collision/DifficultySelection

---
+++ Assets/DifficultyBottle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyBottle : MonoBehaviour {
+
+    public delegate void OnHit(string path);
+    public static OnHit onHit;
+
+    [SerializeField] string path;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            onHit(path);
+        }
+    }
+}
---
+++ Assets/DifficultySelect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultySelect : MonoBehaviour {
+
+    bool selected = false;
+
+    private void Start()
+    {
+        DifficultyBottle.onHit += SetDifficulty;
+    }
+
+    private void Update()
+    {
+        if(GameManager.Instance.PlayerCurrentGameState != GameManager.GameStates.WAITING_TO_START)
+        {
+            Destroy(gameObject);
+        }
+
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            SetDifficulty("StreamingAssets/song_hackathon_medium.xml");
+        }
+    }
+
+
+    void SetDifficulty(string path)
+    {
+        print("setting Difficulty");
+        if (!selected&&GameManager.Instance.PlayerCurrentGameState == GameManager.GameStates.WAITING_TO_START)
+        {
+            GameManager.Instance.SetPath(path);
+            selected = true;
+
+            Destroy(gameObject, 3f);
+        }
+    }
+}
